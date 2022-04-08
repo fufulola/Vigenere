c939685904a74e1f2ca0328c431be44ab2afdc3f
fufu_lola <62877954+fufulola@users.noreply.github.com>
2022-04-08 08:50:20 +0200

    Update tp_v1.py

--- tp_v1.py
+++ tp_v1.py
@@ -8,6 +8,7 @@ cle_in = input("entrez votre clé: ")
 
 #definition des variables
 compteur = 0
+compteur_text2 = 0
 compteurasci_cle = 0
 compteurasci_text = 0 
 asci_text = []
@@ -18,6 +19,7 @@ text_chiffre = []
 caractere_chiffre = 0
 
 
+
 #convertir la clé en ascii et mets dans une liste
 for cle_i in cle_in:
     asci_cle.insert(compteurasci_cle, ord(cle_i))
@@ -26,10 +28,22 @@ for cle_i in cle_in:
 
 #covertir le text en ascii et mets dans une liste
 for text_i in text_in:
+    """
+    if ord(text_i) == 32:
+        asci_text.insert(compteurasci_text, " ")
+        compteurasci_text = compteurasci_text + 1
+    else:
+        print("je passe")
+        asci_text.insert(compteurasci_text, ord(text_i))
+        #print(ord(text_i))
+        #print("-------------------------")
+        compteurasci_text = compteurasci_text + 1
+    """
     asci_text.insert(compteurasci_text, ord(text_i))
     #print(ord(text_i))
     #print("-------------------------")
     compteurasci_text = compteurasci_text + 1
+print(asci_text)
 
 """
 if compteurasci_text < compteurasci_cle :
@@ -41,8 +55,6 @@ else:
     compteurasci_total = compteurasci_text / compteurasci_cle
     print(compteurasci_total)
 """
-
-#prend la valeure la plus petite
 if compteurasci_text < compteurasci_cle :
     compteurasci_total =  compteurasci_cle 
 
@@ -51,22 +63,26 @@ else:
 
 #gros c carré je l'ai reconverti en caracteres pour voir et il me sort sa : ['\x04', '\x16', '\x0c', '\x19', '\x02', '\x07']
 
+
 #chiffre le texte
-for x in range (compteurasci_text): 
+for x in range(compteurasci_total):
 
     #regarde si c'est un espace si oui le converti pas
-    if compteurasci_text[x] == 32:
+    if asci_text[compteur_text2] == 32:
         text_chiffre.insert(x, " ")
+        compteur_text2 = compteur_text2 + 1
 
     #si ce n'est pas un espace le converti
     else:
         caractere_chiffre = (asci_text[x] + asci_cle[x])%26
-        caractere_chiffre = chr(caractere_chiffre)
-        print(caractere_chiffre) 
+        #caractere_chiffre = chr(caractere_chiffre)
+        print(caractere_chiffre)
         #mets le calcul dans la variable
-        text_chiffre.insert(x, caractere_chiffre)
+        text_chiffre.insert(compteur_text2, caractere_chiffre)
+        compteur_text2 = compteur_text2 + 1
     print("------------------")
     print(text_chiffre)
+
 
 #passe le texte chiffré en caractères
 
